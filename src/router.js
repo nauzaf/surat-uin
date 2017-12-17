905384d680463aa1134d4c60f851fba8c39da1ac
Muhammad Fauzan Albaihaqi <mfauzan.albaihaqi@gmail.com>
2017-12-18 05:52:50 +0700

    fix router

--- src/router.js
+++ src/router.js
@@ -1,11 +1,15 @@
 import { StackNavigator } from 'react-navigation'
 
 import KotakMasukPage from './layouts/KotakMasukPage'
+import KotakKeluarPage from './layouts/KotakKeluarPage'
 import LoginPage from './layouts/LoginPage'
+import TulisSuratPage from './layouts/TulisSuratPage'
 
 const router = StackNavigator({
     Login: { screen: LoginPage },
-    KotakMasuk: { screen: KotakMasukPage }
+    KotakMasuk: { screen: KotakMasukPage },
+    KotakKeluar: { screen: KotakKeluarPage },
+    TulisSurat: { screen: TulisSuratPage }
 })
 
 export default router
