b87edaf0676f71a69420dea6d732bb764b7c29fb
Muhammad Fauzan Albaihaqi <mfauzan.albaihaqi@gmail.com>
2017-12-28 12:54:24 +0700

    fix list surat

--- src/components/ListSurat.js
+++ src/components/ListSurat.js
@@ -14,8 +14,8 @@ export default class ListSurat extends Component{
               <Thumbnail style={{marginLeft:12}} source={require('../assets/img/profile_default.png')} />
             </Left>
             <Body >
-              <Text onPress={()=> alert('ini surat masuk')}>{ this.props.asalSurat }</Text>
-              <Text note onPress={()=> alert('ini surat masuk')}>{ this.props.catatan }</Text>
+              <Text onPress={this.props.navigator}>{ this.props.asalSurat }</Text>
+              <Text note onPress={this.props.navigator}>{ this.props.catatan }</Text>
             </Body>
             <Right>
               <Text note>{ this.props.tglMasuk }</Text>
